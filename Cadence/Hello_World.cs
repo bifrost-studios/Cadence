ee5174e86cee1bf72e78ab2a734bb624210045e1
Nicholas Keen <nrk_1996@yahoo.com>
2022-07-12 14:33:02 -0400

    Add bot onlining functionality
    The code here allows the bot to come online and is complete with basic error logging and stores important information in external configuration files.
    
    Issue: #1

--- Cadence/Hello_World.cs
+++ Cadence/Hello_World.cs
@@ -1,7 +1,57 @@
-﻿namespace Cadence
-{
-    public class Hello_World
-    {
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Discord;
+using Discord.WebSocket;
+using NLog;
+using NLog.Extensions.Logging;
 
+namespace Cadence
+{
+    public class HelloWorld
+    {
+        private static readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        private static readonly Logger Logger = LogManager.GetLogger(AppName + ".log");
+        public static Task Main() => new HelloWorld().MainAsync();
+
+        private DiscordSocketClient DiscordClient;
+
+        public async Task MainAsync()
+        {
+
+            IConfiguration Config = new ConfigurationBuilder()
+            .AddJsonFile("C:\\Config\\Cadence.json")
+            .AddJsonFile("C:\\Config\\NLog.json")
+            .Build();
+
+            Config.GetSection("NLog:targets:MainLogFile:fileName").Value += Config["LogFile"];
+            Config.GetSection("NLog:targets:MainLogFile:archiveFileName").Value += Config["ArchiveLogFile"];
+            Config.GetSection("NLog:rules:logging:minLevel").Value += Config["LogLevel"];
+
+            LogManager.Configuration = new NLogLoggingConfiguration(Config.GetSection("NLog"));
+
+            try
+            {
+
+                DiscordClient = new DiscordSocketClient();
+                DiscordClient.Log += Log;
+
+                var Token = File.ReadAllText(Config["Token"]).Trim();
+
+                await DiscordClient.LoginAsync(TokenType.Bot, Token);
+                await DiscordClient.StartAsync();
+
+                await Task.Delay(-1);
+
+            }catch (Exception E)
+            {
+                await Log(new LogMessage(LogSeverity.Error, E.Source, E.Message, E));
+            }
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Logger.Error(message.ToString());
+            return Task.CompletedTask;
+        }
     }
 }
