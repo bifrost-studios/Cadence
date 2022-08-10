df9347f13a54ccf26474ceabdd5fd03c92bea4e3
Nicholas Keen <nrk_1996@yahoo.com>
2022-08-10 18:52:05 -0400

    Add Modules class
    This class is the repository for commands.
    
    Issue: #1

---
+++ Cadence/InfoModule.cs
@@ -0,0 +1,8 @@
+﻿using Discord.Commands;
+
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+    [Command("Hello")]
+    [Summary("Replies with \"World!\"")]
+    public Task HelloAsync() => ReplyAsync("World!");
+}
